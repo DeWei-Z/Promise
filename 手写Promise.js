ef492e9b1bc7ad8c409219e0eb0c61fe3983e5fa
dewei zhang <377805163@qq.com>
2021-04-20 17:12:29 +0800

    Promise race

--- 手写Promise.js
+++ 手写Promise.js
@@ -154,6 +154,19 @@
               }
             })
         }
+
+        Promise.race=function(promises){
+            return new Promise((resolve,reject)=>{
+
+                for(let i=0;i<promises.length;i++){
+                    promises[i].then(value=>{
+                        resolve(value)
+                    },reason=>{
+                        reject(reason)
+                    })
+                }
+            })
+        }
      
                    
                 

235b0cda3d43e9fe865ff0f2e4deb6a99325bade
dewei zhang <377805163@qq.com>
2021-04-20 10:55:09 +0800

    Promise all

--- 手写Promise.js
+++ 手写Promise.js
@@ -135,6 +135,25 @@
                 reject(reason)
             })
         }
+
+        Promise.all=function(promises){
+            return new Promise((resolve,reject)=>{
+                let count=0
+                let arr=[]
+
+              for(let i=0;i<promises.length;i++){
+                promises[i].then(value=>{
+                    count++
+                    arr[i]=value
+                    if(count===promises.length){
+                        resolve(arr)
+                    }
+                },reason=>{
+                    reject(reason)
+                })
+              }
+            })
+        }
      
                    
                 
